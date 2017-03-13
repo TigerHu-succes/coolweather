b349c4cbad3c38ef370b1f6280ce8f2c05f7d339
Tiger <739182926@qq.com>
2017-03-13 19:31:03 +0800

    tiger

---
+++ app/src/main/java/com/example/coolweather/util/MyApplication.java
@@ -0,0 +1,28 @@
+package com.example.coolweather.util;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+
+public class MyApplication extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context=getApplicationContext();
+        LitePalApplication.initialize(context);
+    }
+
+    public Context getContext(){
+
+        return context;
+    }
+
+}
